b017ffdf9605b12afd50f28e6b07a9c4a9a80f2a
nickdoxa <nick091702@gmail.com>
2024-07-10 22:40:32 -0400

    Updated Home Controller and result.jsp

--- src/main/java/net/oasisgames/budgetcalculator/controllers/HomeController.java
+++ src/main/java/net/oasisgames/budgetcalculator/controllers/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 /**
@@ -38,7 +40,7 @@ public class HomeController {
             double monthlyInsuranceDouble = Double.parseDouble(monthlyInsurance);
             double monthlyPhoneDouble = Double.parseDouble(monthlyPhone);
             double weeklyTransportDouble = Double.parseDouble(weeklyTransport);
-            double afterExpenses = calculateTotalAfterTax(new double[] {monthlyRentDouble,
+            double afterExpenses = calculateTotalRemainder(new double[] {monthlyRentDouble,
                             monthlyStreamingDouble, carPaymentDouble, monthlyInsuranceDouble,
                             monthlyPhoneDouble}, new double[] {weeklyFoodDouble,
                             weeklyTransportDouble}, annualIncomeDouble);
@@ -47,8 +49,18 @@ public class HomeController {
                         "Internal error occurred while calculating total!");
                 return "index";
             }
-            model.addAttribute("result", "worked!");
-            model.addAttribute("total", afterExpenses);
+            if (afterExpenses < 0) {
+                model.addAttribute("errorText",
+                        "Your expenses add up to more than what your " +
+                                "annual income covers. Try adjusting your budget!");
+                return "index";
+            }
+            //SET RESULT PAGE ATTRIBUTES HERE
+            setTotalTaxDiv(model, annualIncomeDouble);
+            setTotalExpensesDiv(model, monthlyRentDouble, weeklyFoodDouble,
+                    monthlyStreamingDouble, carPaymentDouble, monthlyInsuranceDouble,
+                    monthlyPhoneDouble, weeklyTransportDouble);
+            setRemainingIncomeDiv(model, afterExpenses, annualIncomeDouble);
         } catch (NumberFormatException e) {
             model.addAttribute("errorText",
                     "One or more fields contains invalid characters or is empty!");
@@ -64,7 +76,7 @@ public class HomeController {
      * @param income the users total annual income
      * @return the users remaining income
      */
-    private double calculateTotalAfterTax(double[] monthly, double[] weekly, double income) {
+    private double calculateTotalRemainder(double[] monthly, double[] weekly, double income) {
         double tax = calculateFederalTax(income);
         double totalExpense = calculateTotalExpenses(monthly, weekly);
         if (totalExpense == -1) return -1;
@@ -82,11 +94,11 @@ public class HomeController {
         double totalMonth = Arrays.stream(monthly)
                 .map(i -> i * 12)
                 .reduce(Double::sum)
-                .orElseGet(() -> -1);
+                .orElse(-1);
         double totalWeekly = Arrays.stream(weekly)
-                .map(i -> (i * 4) * 12)
+                .map(i -> i * 48)
                 .reduce(Double::sum)
-                .orElseGet(() -> -1);
+                .orElse(-1);
         return totalMonth + totalWeekly;
     }
 
@@ -105,6 +117,92 @@ public class HomeController {
         else if (income > 243725 && income < 609350) tax = 0.35;
         else tax = 0.37;
         return tax;
+    }
+
+    /**
+     * Formats a numeric string to USD currency
+     * @param str string to format to currency (USD)
+     * @return formatted string
+     */
+    private String formatStringToCurrency(String str) {
+        DecimalFormat formatter = new DecimalFormat("$#,##0.00");
+        BigDecimal deci = new BigDecimal(str);
+        return formatter.format(deci);
+    }
+
+    /**
+     * Sets the parameters of the Total Tax Div in result.jsp
+     * @param model Spring Model
+     * @param annualIncomeDouble users annual income
+     */
+    private void setTotalTaxDiv(Model model, double annualIncomeDouble) {
+        model.addAttribute("totalIncome",
+                formatStringToCurrency(annualIncomeDouble + ""));
+        double tax = calculateFederalTax(annualIncomeDouble);
+        String taxToString = (tax * 100) + "";
+        model.addAttribute("totalTaxPercent", taxToString);
+        model.addAttribute("tax", taxToString);
+        double totalTax = annualIncomeDouble * tax;
+        model.addAttribute("totalTaxAmount",
+                formatStringToCurrency(totalTax + ""));
+        double totalIncomeAfterTax = annualIncomeDouble - totalTax;
+        model.addAttribute("totalIncomeAfterTax",
+                formatStringToCurrency(totalIncomeAfterTax + ""));
+    }
+
+    /**
+     * Sets the parameters of the Total Expenses Div in result.jsp
+     * @param model Spring Model
+     * @param rent monthly rent price
+     * @param food weekly food cost
+     * @param streaming monthly streaming cost
+     * @param car monthly car payment
+     * @param insurance monthly insurance bills
+     * @param phone monthly phone bill
+     * @param transport weekly transportation costs
+     */
+    private void setTotalExpensesDiv(Model model, double rent, double food,
+                                     double streaming, double car, double insurance,
+                                     double phone, double transport) {
+        String annualRent = formatStringToCurrency((rent * 12) + "");
+        String annualFood = formatStringToCurrency((food * 48) + "");
+        String annualStreaming = formatStringToCurrency((streaming * 12) + "");
+        String annualCar = formatStringToCurrency((car * 12) + "");
+        String annualInsurance = formatStringToCurrency((insurance * 12) + "");
+        String annualPhone = formatStringToCurrency((phone * 12) + "");
+        String annualTransport = formatStringToCurrency((transport * 48) + "");
+        model.addAttribute("annualRent", annualRent);
+        model.addAttribute("annualFood", annualFood);
+        model.addAttribute("annualStreaming", annualStreaming);
+        model.addAttribute("annualCarPayment", annualCar);
+        model.addAttribute("annualInsurance", annualInsurance);
+        model.addAttribute("annualPhone", annualPhone);
+        model.addAttribute("annualTransport", annualTransport);
+    }
+
+    /**
+     * Sets the parameters of the Remaining Income Div in result.jsp
+     * @param model Spring Model
+     * @param remainder the users remaining income after taxes and expenses
+     * @param income the users annual income
+     */
+    private void setRemainingIncomeDiv(Model model, double remainder, double income) {
+        model.addAttribute("totalIncomeRemainder",
+                formatStringToCurrency(remainder + ""));
+        boolean highRemainder = remainder > income * 0.2;
+        String investMessage = highRemainder ? "You have a high enough remainder that" +
+                " you can invest 20% of your post-tax income into savings!" : "Your remainder is" +
+                " not high enough to invest 20% of your post-tax income into savings!";
+        model.addAttribute("investMessage", investMessage);
+        String savingsDeposit = highRemainder ?
+                formatStringToCurrency(((income * 0.2) / 12) + "") :
+                formatStringToCurrency((remainder * 0.75) / 12 + "");
+        model.addAttribute("savingsDeposit", savingsDeposit);
+        if (highRemainder) return;
+        model.addAttribute("lowRemainderMessage",
+                "Since your remaining balance was below the 20% income threshold " +
+                        "we estimated your monthly savings deposit using 75% of your remaining " +
+                        "income!");
     }
 
 }
--- src/main/webapp/views/result.jsp
+++ src/main/webapp/views/result.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <html>
   <head>
     <title>Budget Calculator</title>
@@ -9,9 +9,37 @@
     <script src="../assets/js/jquery.min.js"></script>
   </head>
   <body>
-    <div class="align-center box style3">
+    <div class="align-center box style3" style="width: 60%; margin:auto;">
       <h2>Budget Results:</h2>
-      <p>${result} ${total}</p>
+      <br>
+      <div class="style1 align-left">
+        <h3><u>Total Taxes</u></h3>
+        <p>With a total income of ${totalIncome} your federal income tax is ${totalTaxPercent}% which
+        takes ${totalTaxAmount} and makes your take home income ${totalIncomeAfterTax}</p>
+      </div>
+      <br>
+      <div class="style1 align-left">
+        <h3><u>Total Expenses</u></h3>
+        <p>This is how your expenses line up annually against your salary:</p>
+        <ul>
+          <li>Rent/Mortgage Price: ${annualRent}</li>
+          <li>Food Spending: ${annualFood}</li>
+          <li>Streaming/Cable Price: ${annualStreaming}</li>
+          <li>Car Payment: ${annualCarPayment}</li>
+          <li>Insurance Costs: ${annualInsurance}</li>
+          <li>Phone Bill: ${annualPhone}</li>
+          <li>Transportation Costs: ${annualTransport}</li>
+        </ul>
+      </div>
+      <div class="style1 align-center">
+        <h3><u>Remaining Income:</u></h3>
+        <p>After paying all of your expenses and the federal income tax you will be left with
+        ${totalIncomeRemainder}. It is suggested by most financial advisors to invest 20%
+        of your income after taxes into savings. Many banks offer a High Yield Savings Account which gives
+        around a 5% interest rate. ${investMessage}.</p><br>
+        <p>Your recommended monthly saving deposit is: ${savingsDeposit}</p>
+        <p>${lowRemainderMessage}</p>
+      </div>
     </div>
   </body>
 </html>
